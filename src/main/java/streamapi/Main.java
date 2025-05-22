965f87720badb352fc97ef441da381dbe701ac28
Louis Freier <Louisjamesfreier@gmail.com>
2025-05-22 13:57:05 +0200

    task_iii: Zufallszahlen mit Stream-API generieren

--- src/main/java/streamapi/Main.java
+++ src/main/java/streamapi/Main.java
@@ -2,6 +2,7 @@ package streamapi;
 
 import java.io.InputStream;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /** Starter for the stream api task. */
 public class Main {
@@ -59,21 +60,13 @@ public class Main {
     public static List<Integer> random() {
         Random r = new Random();
 
-        // TODO
-        List<Integer> randomIntegers = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            randomIntegers.add(r.nextInt(10));
-        }
-
-        List<Integer> returnList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            if (randomIntegers.get(i) % 2 == 0) {
-                returnList.add(randomIntegers.get(i) * randomIntegers.get(i));
-            }
-        }
-
-        return returnList;
+        return r.ints(10, 0, 10)             // 10 Zufallszahlen von 0 bis 9
+            .filter(i -> i % 2 == 0)      // nur gerade Zahlen
+            .map(i -> i * i)              // quadrieren
+            .boxed()                      // IntStream -> Stream<Integer>
+            .collect(Collectors.toList());  // sammeln in Liste
     }
+
 
     /**
      * Task IV: Open resources.
