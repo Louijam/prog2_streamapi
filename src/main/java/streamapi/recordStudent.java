2a706e15b360855d103fa8c6cc206a87edb0b330
Louis Freier <Louisjamesfreier@gmail.com>
2025-05-22 14:40:50 +0200

    Task Record: Ersetze Student-Klasse durch Record

---
+++ src/main/java/streamapi/recordStudent.java
@@ -0,0 +1,14 @@
+package streamapi;
+
+/** Represent a student. */
+public record recordStudent(String name, int cps, Enrollment program) {
+
+    /**
+     * Check if a student is enrolled in Computer Science.
+     *
+     * @return {@code true}, iff the student is enrolled in Computer Science, otherwise {@code false}
+     */
+    public boolean isIFM() {
+        return program == Enrollment.IFM;
+    }
+}
